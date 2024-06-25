a7b6c2ee9b319410cf721b036f680b46c8397f73
divergen371 <51857554+divergen371@users.noreply.github.com>
2024-06-26 01:13:20 +0900

    署名の検証に失敗する

--- src/main/scala/ECCTest.scala
+++ src/main/scala/ECCTest.scala
@@ -15,43 +15,42 @@ object ECCTest {
       "FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEFFFFFC2F",
       16
     )
-
-    // 楕円曲線を定義
-    val curve = new EllipticCurve(a, b, p)
     val basePoint = Point(
       BigInt(
-        "55066263022277343669578718895168534326250603453777594175500187360389116729240"
+        "6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296",
+        16
       ),
       BigInt(
-        "32670510020758816978083085130507043184471273380659243275938904335757337482424"
+        "4fe342e2fe1a7f9b8ee7eb4a7c0f9e162cbddce1009244c8f911c7c2b1c5994f",
+        16
       )
     )
-
-    // ランダムなスカラー値を秘密鍵とし、それを基点にスカラー倍を行って公開鍵を生成
-    val (privateKey, publicKey) = curve.generateKeyPair(basePoint)
+    // 鍵ペアを生成
+    val (privateKey, publicKey) = EllipticCurve.generateKeyPair(basePoint, a, p)
 
     println(s"Private Key: $privateKey")
     println(s"Public Key: (${publicKey.x}, ${publicKey.y})")
 
     // メッセージの暗号化と復号
-
     val message = Point(BigInt("12345"), BigInt("67890"))
-    val ciphertext = curve.encrypt(publicKey, message, basePoint)
+    val ciphertext = EllipticCurve.encrypt(publicKey, message, basePoint, a, p)
     println(
       s"Ciphertext: (${ciphertext._1.x}, ${ciphertext._1.y}), (${ciphertext._2.x}, ${ciphertext._2.y})"
     )
 
-    val decryptedMessage = curve.decrypt(privateKey, ciphertext)
+    val decryptedMessage = EllipticCurve.decrypt(privateKey, ciphertext, a, p)
     println(
       s"Decrypted Message: (${decryptedMessage.x}, ${decryptedMessage.y})"
     )
 
     // メッセージの署名と検証
     val signMessage = "Hello, ECC!".getBytes("UTF-8")
-    val signature = curve.sign(privateKey, signMessage, basePoint)
+    val signature = EllipticCurve
+      .sign(privateKey, signMessage, basePoint, a, p)
     println(s"Signature: (r: ${signature._1}, s: ${signature._2})")
 
-    val isValid = curve.verify(publicKey, signMessage, signature, basePoint)
+    val isValid = EllipticCurve
+      .verify(publicKey, signMessage, signature, basePoint, a, p)
     println(s"Signature valid: $isValid")
   }
 }
--- src/main/scala/EllipticCurve.scala
+++ src/main/scala/EllipticCurve.scala
@@ -1,26 +1,29 @@
 import java.security.{MessageDigest, SecureRandom}
 
-/** 楕円曲線を表すクラス。
-  *
-  * @param a
-  *   楕円曲線のパラメータa
-  * @param b
-  *   楕円曲線のパラメータb
-  * @param p
-  *   素数p（有限体のモジュラス）
-  */
-class EllipticCurve(a: BigInt, b: BigInt, p: BigInt) {
+object EllipticCurve {
+  private val random = new SecureRandom()
 
   /** 鍵ペアを生成。
     *
     * @param basePoint
     *   基点
+    *
+    * @param a
+    *   楕円曲線のパラメータa
+    *
+    * @param p
+    *   素数p（有限体のモジュラス）
+    *
     * @return
     *   秘密鍵と公開鍵のペア
     */
-  def generateKeyPair(basePoint: Point): (BigInt, Point) = {
-    val privateKey = generateRandomScalar()
-    val publicKey = scalarMultiply(privateKey, basePoint)
+  def generateKeyPair(
+      basePoint: Point,
+      a: BigInt,
+      p: BigInt
+  ): (BigInt, Point) = {
+    val privateKey = generateRandomScalar(p)
+    val publicKey = scalarMultiply(privateKey, basePoint, a, p)
     (privateKey, publicKey)
   }
 
@@ -28,74 +31,128 @@ class EllipticCurve(a: BigInt, b: BigInt, p: BigInt) {
     *
     * @param publicKey
     *   受信者の公開鍵
+    *
     * @param message
     *   暗号化するメッセージ（点として表現）
+    *
+    * @param basePoint
+    *   基点
+    *
+    * @param a
+    *   楕円曲線のパラメータa
+    *
+    * @param p
+    *   素数p（有限体のモジュラス）
+    *
     * @return
     *   暗号化されたメッセージ（点のペア）
     */
   def encrypt(
       publicKey: Point,
       message: Point,
-      basePoint: Point
+      basePoint: Point,
+      a: BigInt,
+      p: BigInt
   ): (Point, Point) = {
-    val k = generateRandomScalar()
-    val c1 = scalarMultiply(k, basePoint)
-    val c2 = addPoints(message, scalarMultiply(k, publicKey))
+    val k = generateRandomScalar(p)
+    val c1 = scalarMultiply(k, basePoint, a, p)
+    val c2 = addPoints(message, scalarMultiply(k, publicKey, a, p), a, p)
     (c1, c2)
   }
 
   /** ランダムなスカラー値を生成。
     *
+    * @param p
+    *   素数p（有限体のモジュラス）
+    *
     * @return
     *   ランダムなスカラー値
     */
-  def generateRandomScalar(): BigInt = {
-    val random = new SecureRandom()
-    BigInt.probablePrime(256, random).mod(p)
+  def generateRandomScalar(p: BigInt): BigInt = {
+    BigInt(256, random).mod(p)
+  }
+
+  /** メッセージの復号。
+    *
+    * @param privateKey
+    *   受信者の秘密鍵
+    *
+    * @param ciphertext
+    *   暗号化されたメッセージ（点のペア）
+    *
+    * @param a
+    *   楕円曲線のパラメータa
+    *
+    * @param p
+    *   素数p（有限体のモジュラス）
+    *
+    * @return
+    *   復号されたメッセージ（点）
+    */
+  def decrypt(
+      privateKey: BigInt,
+      ciphertext: (Point, Point),
+      a: BigInt,
+      p: BigInt
+  ): Point = {
+    val (c1, c2) = ciphertext
+    val s = scalarMultiply(privateKey, c1, a, p)
+    addPoints(c2, Point(s.x, -s.y), a, p)
   }
 
   /** スカラー倍（点の乗算）を実装。
     *
     * @param k
     *   スカラー値
-    * @param p
+    *
+    * @param point
     *   楕円曲線上の点
+    *
+    * @param a
+    *   楕円曲線のパラメータa
+    *
+    * @param p
+    *   素数p（有限体のモジュラス）
+    *
     * @return
     *   スカラー倍された点
     */
-  def scalarMultiply(k: BigInt, p: Point): Point = {
-    var n = k
-    var q = p
-    var result = Point(0, 0) // 無限遠点
-
-    while (n > 0) {
-      if ((n & 1) == 1) {
-        result = addPoints(result, q)
-      }
-      q = addPoints(q, q)
-      n >>= 1
+  def scalarMultiply(k: BigInt, point: Point, a: BigInt, p: BigInt): Point = {
+    @scala.annotation.tailrec
+    def loop(n: BigInt, q: Point, acc: Point): Point = {
+      if (n == 0) acc
+      else if ((n & 1) == 1)
+        loop(n >> 1, addPoints(q, q, a, p), addPoints(acc, q, a, p))
+      else loop(n >> 1, addPoints(q, q, a, p), acc)
     }
-    result
+
+    loop(k, point, Point(0, 0))
   }
 
   /** 2つの楕円曲線上の点を加算する。
     *
     * @param p1
     *   加算する最初の点
+    *
     * @param p2
     *   加算する2番目の点
+    *
+    * @param a
+    *   楕円曲線のパラメータa
+    *
+    * @param p
+    *   素数p（有限体のモジュラス）
+    *
     * @return
     *   加算結果の点
     */
-  def addPoints(p1: Point, p2: Point): Point = {
+  def addPoints(p1: Point, p2: Point, a: BigInt, p: BigInt): Point = {
     if (p1 == p2) {
-      // p1とp2が同じ点の場合
       val m = (3 * p1.x.pow(2) + a) * (2 * p1.y).modInverse(p) % p
       val x3 = (m.pow(2) - 2 * p1.x) % p
       val y3 = (m * (p1.x - x3) - p1.y) % p
       Point(x3, y3)
     } else {
-      // p1とp2が異なる点の場合
       val m = (p2.y - p1.y) * (p2.x - p1.x).modInverse(p) % p
       val x3 = (m.pow(2) - p1.x - p2.x) % p
       val y3 = (m * (p1.x - x3) - p1.y) % p
@@ -103,41 +160,38 @@ class EllipticCurve(a: BigInt, b: BigInt, p: BigInt) {
     }
   }
 
-  /** メッセージの復号。
-    *
-    * @param privateKey
-    *   受信者の秘密鍵
-    * @param ciphertext
-    *   暗号化されたメッセージ（点のペア）
-    * @return
-    *   復号されたメッセージ（点）
-    */
-  def decrypt(privateKey: BigInt, ciphertext: (Point, Point)): Point = {
-    val (c1, c2) = ciphertext
-    val s = scalarMultiply(privateKey, c1)
-    val message = addPoints(c2, Point(s.x, -s.y))
-    message
-  }
-
   /** メッセージの署名を生成。
     *
     * @param privateKey
     *   署名者の秘密鍵
+    *
     * @param message
     *   署名するメッセージ
+    *
+    * @param basePoint
+    *   基点
+    *
+    * @param a
+    *   楕円曲線のパラメータa
+    *
+    * @param p
+    *   素数p（有限体のモジュラス）
+    *
     * @return
     *   署名（r, s）
     */
   def sign(
       privateKey: BigInt,
       message: Array[Byte],
-      basePoint: Point
+      basePoint: Point,
+      a: BigInt,
+      p: BigInt
   ): (BigInt, BigInt) = {
-    val random = new SecureRandom()
-    val k = BigInt.probablePrime(256, random).mod(p)
-    val r = scalarMultiply(k, basePoint).x.mod(p)
-    val z = BigInt(MessageDigest.getInstance("SHA-256").digest(message)).mod(p)
-    val s = ((z + r * privateKey) * k.modInverse(p)).mod(p)
+    val k = generateRandomScalar(p)
+    val r = scalarMultiply(k, basePoint, a, p).x.mod(p)
+    val z = BigInt(1, MessageDigest.getInstance("SHA-256").digest(message))
+      .mod(p)
+    val s = (k.modInverse(p) * (z + r * privateKey)).mod(p)
     (r, s)
   }
 
@@ -145,10 +199,22 @@ class EllipticCurve(a: BigInt, b: BigInt, p: BigInt) {
     *
     * @param publicKey
     *   署名者の公開鍵
+    *
     * @param message
     *   検証するメッセージ
+    *
     * @param signature
     *   検証する署名（r, s）
+    *
+    * @param basePoint
+    *   基点
+    *
+    * @param a
+    *   楕円曲線のパラメータa
+    *
+    * @param p
+    *   素数p（有限体のモジュラス）
+    *
     * @return
     *   署名が正しければtrue、そうでなければfalse
     */
@@ -156,15 +222,28 @@ class EllipticCurve(a: BigInt, b: BigInt, p: BigInt) {
       publicKey: Point,
       message: Array[Byte],
       signature: (BigInt, BigInt),
-      basePoint: Point
+      basePoint: Point,
+      a: BigInt,
+      p: BigInt
   ): Boolean = {
     val (r, s) = signature
-    val z = BigInt(MessageDigest.getInstance("SHA-256").digest(message)).mod(p)
+    val z = BigInt(
+      1,
+      MessageDigest
+        .getInstance("SHA-256")
+        .digest(message)
+    )
+      .mod(p)
     val w = s.modInverse(p)
     val u1 = (z * w).mod(p)
     val u2 = (r * w).mod(p)
-    val point =
-      addPoints(scalarMultiply(u1, basePoint), scalarMultiply(u2, publicKey))
+    val point = addPoints(
+      scalarMultiply(u1, basePoint, a, p),
+      scalarMultiply(u2, publicKey, a, p),
+      a,
+      p
+    )
     r == point.x.mod(p)
   }
+
 }
